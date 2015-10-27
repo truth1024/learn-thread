7a1e48671d4577e9c2768417b5b0f89d9170cf22
truth1024 <truth1024@hotmail.com>
2015-10-27 16:04:33 +0800

    init commit

---
+++ src/main/java/com/truth99/learn/thread/Learn.java
@@ -0,0 +1,10 @@
+package com.truth99.learn.thread;
+
+/**
+ * Created by LD on 2015/10/27 0027.
+ */
+public class Learn {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+    }
+}
